5c83a0e350a79ca562cddf36551d00f0fd2e24ff
Virginie Dourson <virginiedourson@gmail.com>
2022-12-09 14:01:16 +0100

    Exercice 1 corrigé

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //Quand on entre une lettre dans le 1er input, elle est recopiée en dessous de la zone d'insertion.
-
+let firstname = document.getElementById('firstname');
 let displayFirstname = document.getElementById('display-firstname');
 let displayFirstnameText = document.createTextNode("");
 displayFirstname.appendChild(displayFirstnameText);
 firstname.addEventListener('keyup', (e) => {
-    displayFirstnameText.textContent = document.getElementById('firstname').value;
+    displayFirstnameText.textContent = firstname.value;
 });
+
+//Lorsque l'age est encodé dans le second input, si il est supérier ou égal à 18,
+//le contenu de la section "a-hard-truth" est révélé.
